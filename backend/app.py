2e56faabcbce55111cad5c2299638ff0a1b4bea6
Salil Upadhyay <upsalil.01@gmail.com>
2023-07-04 17:57:32 +0530

    completed order backend

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import os
 import uuid
 import jwt
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,g
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,6 +17,7 @@ app.config['SECRET_KEY'] = secretKey
 # global variables to store user and food data
 existing_data = []
 food_items = []
+orders = []
 data_file_path = "./inventory.json"
 
 # ----------- data loading from local system --------------
@@ -58,11 +59,25 @@ def load_data():
     except FileNotFoundError:
         return []
 
+# load orders
+def load_orders():
+    global orders
+    try:
+        with open("orders.json", "r") as file:
+            data = json.load(file)
+            orders = data
+    except FileNotFoundError:
+        orders = []
 
 # save food data
 def save_data(data):
     with open(data_file_path, 'w') as file:
-        json.dump(data, file)
+        json.dump(data, file,indent=4)
+
+# save orders
+def save_orders():
+    with open("orders.json", "w") as file:
+        json.dump(orders, file,indent=4)
 
 
 # initialize data on server run
@@ -102,6 +117,8 @@ def authenticate_and_authorize(role):
                     return jsonify({'message': 'Unauthorized access'}), 403
 
                 # Perform the protected action
+                # using g module of flask to attach mail to request object
+                g.user_email = payload["email"]
                 return func(*args, **kwargs)
 
             except jwt.ExpiredSignatureError:
@@ -152,25 +169,77 @@ def login():
 
     return jsonify({'message': 'Invalid credentials'}), 401
 
+# non protected routes
 # menu route to show items avaialable to user
 @app.route('/menu', methods=['GET'])
 def get_menu():
-    check_food_items_exist()
+    initialize_food_items()
     menu_items = [item for item in food_items if item['availability'] == 'Yes']
     return jsonify(menu_items)
+
+# multiple order taking facility
+@app.route('/place-order', methods=['POST'])
+def place_order():
+    # Get data from the request body
+    user_email = g.user_email
+    data = request.json
+    order_items = data.get('items', [])
+    load_orders()
+    # Validate the order items
+    if not isinstance(order_items, list) or len(order_items) == 0:
+        return jsonify({'message': 'Invalid order items'}), 400
+
+    total_price = 0
+
+    # Process each order item
+    for item in order_items:
+        item_id = item.get('item_id')
+        quantity = item.get('quantity')
+
+        # Find the selected food item
+        food_item = next((item for item in food_items if item['id'] == item_id), None)
+
+        # Check if the food item exists
+        if food_item is None:
+            return jsonify({'message': 'Invalid food item ID'}), 400
+
+        # Check if the food item is available
+        if food_item['availability'] != 'Yes':
+            return jsonify({'message': 'Food item is not available'}), 400
+
+        # Check if the quantity is greater than the available stock
+        if quantity > food_item['stock']:
+            return jsonify({'message': 'Insufficient stock'}), 400
+
+        # Calculate the total price for the current item
+        item_price = food_item['price'] * quantity
+        total_price += item_price
+
+        # Update the stock of the food item
+        food_item['stock'] -= quantity
+
+    # Return the response with the total price
+    order = {
+        "order_id": len(orders) + 1,
+        "customer": user_email,
+        "total_price": total_price,
+        "status": "Received"
+    }
+    orders.append(order)
+    return jsonify({'message': 'Order placed successfully', 'total_price': total_price}), 200
 
 
 # protected routes with admin access only 
 # list of all the items avaialable in inventory
 @app.route("/dish",methods=["GET"])
-@authenticate_and_authorize("admin")
+@authenticate_and_authorize("Admin")
 def get_all_items():
     initialize_food_items()
     return jsonify(food_items), 200
 
 # create a food item
 @app.route("/dish",methods=["POST"])
-@authenticate_and_authorize("admin")
+@authenticate_and_authorize("Admin")
 def create_dish():
     initialize_food_items()
     data = request.get_json()
@@ -188,7 +257,7 @@ def create_dish():
 
 # update dish
 @app.route('/dish/<int:dish_id>', methods=['PATCH'])
-@authenticate_and_authorize("admin")
+@authenticate_and_authorize("Admin")
 def update_dish(dish_id):
     initialize_food_items()()
     dish = find_dish_by_id(dish_id)
@@ -209,8 +278,14 @@ def update_dish(dish_id):
     # Save the updated data to the JSON file
     save_data(food_items)
 
-    return jsonify({'message': 'Dish updated successfully'})
+    return jsonify({'message': 'Dish updated successfully'}), 200
 
+# all orders
+@app.route("/orders", methods=["GET"])
+@authenticate_and_authorize("Admin")
+def display_orders():
+   load_orders()
+   return jsonify(orders),200
 
 
 
