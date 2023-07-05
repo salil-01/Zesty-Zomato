6f698d4fa49e3b6a895fb2d8d2120d2239ae24c4
Salil Upadhyay <upsalil.01@gmail.com>
2023-07-05 15:03:09 +0530

    status update completed fe

--- frontend/src/components/Orders.jsx
+++ frontend/src/components/Orders.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import {
   Box,
   Select,
+  Spinner,
   Table,
   TableContainer,
   Tbody,
@@ -32,7 +33,7 @@ function OrdersPage() {
       .then((response) => response.json())
       .then((data) => {
         setOrders(data);
-        console.log(data);
+        // console.log(data);
         setLoading(false);
       })
       .catch((error) => {
@@ -44,71 +45,100 @@ function OrdersPage() {
     fetchData();
   }, []);
 
-  const handleStatusChange = async (orderId, status) => {
-    try {
-      await axios.patch(`/api/orders/${orderId}`, { status });
-      toast({
-        title: "Order updated",
-        status: "success",
-        duration: 3000,
-        isClosable: true,
+  const handleStatusChange = (orderId, status) => {
+    // console.log(orderId, status);
+    orderId = +orderId;
+    setLoading(true);
+    fetch(`${url}/orders/${orderId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ status }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        toast({
+          title: "Order Updated Successfully",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+        });
+        fetchData();
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+        toast({
+          title: "Error while Updating Order",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
       });
-      fetchData();
-    } catch (error) {
-      console.error("Error updating order:", error);
-      toast({
-        title: "Error",
-        description: "Failed to update order",
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-      });
-    }
   };
 
   const toast = useToast();
 
   return (
-    <Box p={4}>
-      <TableContainer margin={"auto"} width={"70%"}>
-        <Table variant="striped" colorScheme="teal">
-          <Thead>
-            <Tr>
-              <Th fontSize={"0.9rem"}>Order ID</Th>
-              <Th fontSize={"0.9rem"}>Name</Th>
-              <Th fontSize={"0.9rem"}>Price</Th>
-              <Th fontSize={"0.9rem"}>Status</Th>
-              <Th fontSize={"0.9rem"} textAlign={"center"}>
-                Action
-              </Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {orders.map((order) => (
-              <Tr key={order.order_id}>
-                <Td>{order.order_id}</Td>
-                <Td>{order.customer}</Td>
-                <Td>$ {order.total_price}</Td>
-                <Td>{order.status}</Td>
-                <Td>
-                  <Select
-                    // width={"50%"}
-                    value={order.status}
-                    onChange={(e) =>
-                      handleStatusChange(order.id, e.target.value)
-                    }
-                  >
-                    <option value="Pending">Pending</option>
-                    <option value="Preparing">Preparing</option>
-                    <option value="Completed">Completed</option>
-                  </Select>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
-    </Box>
+    <>
+      {loading ? (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          height="80vh"
+        >
+          <Spinner size="xl" />
+        </Box>
+      ) : (
+        <Box p={4}>
+          <TableContainer margin={"auto"} width={"70%"}>
+            <Table variant="striped" colorScheme="teal">
+              <Thead>
+                <Tr>
+                  <Th fontSize={"0.9rem"}>Order ID</Th>
+                  <Th fontSize={"0.9rem"}>Name</Th>
+                  <Th fontSize={"0.9rem"}>Price</Th>
+                  <Th fontSize={"0.9rem"}>Status</Th>
+                  <Th fontSize={"0.9rem"} textAlign={"center"}>
+                    Action
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {orders.map((order) => (
+                  <Tr key={order.order_id}>
+                    <Td>{order.order_id}</Td>
+                    <Td>{order.customer}</Td>
+                    <Td>$ {order.total_price}</Td>
+                    <Td>{order.status}</Td>
+                    <Td>
+                      <Select
+                        // width={"50%"}
+                        value={order.status}
+                        onChange={(e) =>
+                          handleStatusChange(order.order_id, e.target.value)
+                        }
+                      >
+                        <option value="">Update Status</option>
+                        <option value="Preparing">Preparing</option>
+                        <option value="Ready">Ready</option>
+                        <option value="Pickup">Pickup</option>
+                        <option value="Deleivered">Deleivered</option>
+                      </Select>
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      )}
+    </>
   );
 }
 
